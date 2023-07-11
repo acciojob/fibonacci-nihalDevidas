dd99624da6ba9cc58325c741361919e53a496224
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-11 20:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+  if(num === 1){
+	  return 0;
+  }
+	if(num === 2){
+		return 1;
+	}
+
+	let first = 0, sec = 1, sum = -1;
+	
+	for(let i = 3; i<= num; i++){
+		sum = first+ sec;
+		first = sec;
+		sec = sum;
+	}
+
+	return sum;
 }
 
 module.exports = fibonacci;
